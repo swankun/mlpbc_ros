47850a3c91aabbb0a6e2a1e57473f72121760a56
Wankun Sirichotiyakul <28937533+swankun@users.noreply.github.com>
2021-10-26 15:22:09 -0600

    idapbc + switching works

--- ml_based_controllers/python/linear.py
+++ ml_based_controllers/python/linear.py
@@ -17,7 +17,7 @@ class InertiaWheelPendulum(object):
     def __init__(
         self, m1=1.423, m2=0.577, lc1=0.17/3.0, 
         l1=0.17, I1=0.0, I2=0.00289, b1=0.00, b2=0.00,
-        gamma_1=3.0, gamma_2=35.0, kp=0.01, kv=200.0
+        gamma_1=3.0, gamma_2=30.0, kp=0.001, kv=0.1
     ):
         self.theta = [0.0, 0.0]
         self.theta_0 = [0.0, 0.0]
@@ -51,6 +51,23 @@ class InertiaWheelPendulum(object):
         #     self.theta_0 = [pos[0], pos[1]]
         #     self.homed = True
 
+    def compute_ida_pbc(self):
+        q1 = self.theta[0] - pi
+        q2 = self.theta[1]
+        q1dot = self.theta_dot[0]
+        q2dot = self.theta_dot[1]
+        m3 = self.m1 + self.m2
+        gamma_1 = self.gamma_1
+        gamma_2 = self.gamma_2
+        kp = self.kp
+        kv = self.kv
+        qbar = q2 + gamma_2*q1
+        qbardot = q2dot + gamma_2*q1dot
+        u_es = gamma_1*sin(q1) + kp*sin(qbar)
+        u_di = -kv*qbardot
+
+        return u_es + u_di
+
     def compute_lqr(self):
         q1 = self.theta[0] #- self.theta_0[0] 
         q2 = self.theta[1] #- self.theta_0[1]
@@ -60,9 +77,22 @@ class InertiaWheelPendulum(object):
         #     self.catch_mode = False
         #     return 0.0
         # K = np.array([-17.22606199290638, -0.31622776601668434, -2.74115091750994, -0.15284235573642457])
-        K = np.array([-52.95441773904679, -0.31622776601755476, -8.433737719677026, -0.421252532338082])
-        x = np.array([sin(q1-pi), rem2pi(q2), q1dot, q2dot])
+        # K = np.array([-5.0395015784599755, -0.010000000000012308, -0.8019036639595001, -0.012460996463504348])
+        K = np.array([-7.409595362575457, -0.05000000000000429, -1.1791663255097424, -0.03665716263249201])
+        x = np.array([sin(q1-pi), sin(q2), q1dot, q2dot])
         return -np.dot(K,x)
+
+    def switching_control(self):
+        q1 = self.theta[0]
+        # q2 = self.theta[1]
+        q1dot = self.theta_dot[0]
+        # q2dot = self.theta_dot[1]
+        if (1+cos(q1)) < (1+cos(pi-pi/10)) and abs(q1dot) < 5.0:
+            self.catch_mode = True
+            return self.compute_lqr()
+        else:
+            self.catch_mode = False
+            return clip(self.compute_ida_pbc(), -0.21, 0.21)
 
 def main():
 
@@ -94,16 +124,18 @@ def main():
     while not rospy.is_shutdown():
         
         torque = 0.0
-        torque = robot.compute_lqr()
+        # torque = robot.compute_lqr()
+        # torque = robot.compute_ida_pbc()
+        torque = robot.switching_control()
 
-        if torque < 0:
+        if torque < 0.0:
             cmd = clip(torque, -max_torque, -deadband) 
-        elif torque > 0:
+        elif torque > 0.0:
             cmd = clip(torque, deadband, max_torque)
         else:
             cmd = 0.0
         gear_ratio = 1.0
-        eta = 0.90 # 0.88
+        eta = 0.95 # 0.88
         k_tau = 0.230    # N-m/a
         current = cmd / gear_ratio / k_tau / eta
         robot.cmd = current
