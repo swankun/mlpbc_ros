b7b4cce7db374f40e2faab7ba80c94a090c833ce
Wankun Sirichotiyakul <28937533+swankun@users.noreply.github.com>
2021-12-15 15:07:45 -0700

    reset home values

--- robot_hardware/src/acrobot_hybrid.cpp
+++ robot_hardware/src/acrobot_hybrid.cpp
@@ -73,11 +73,10 @@ void AcrobotHybrid::disable()
 
 void AcrobotHybrid::reset()
 {
-    joints_[0].position = joints_[0].home;
-    joints_[1].position = joints_[1].home;
+    theta1_homed_ = false;
+    joints_[1].home = epos_device_.readPosition();
     epos_device_.clearErrors();
     epos_device_.writeCurrent(0.0);
-    epos_device_.enableDevice();
 }
 
 void AcrobotHybrid::disableOperation()
